f937e2c4013a2a88d32daf7a366890a772067975
Noha AlDossari <60599498+Noha66@users.noreply.github.com>
2020-09-13 22:49:36 +0300

    Add files via upload
    This is index.js file (after Hompepage edit)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function index({ navigation }) {
     return (
