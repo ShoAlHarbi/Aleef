185cf7d07830f1458f1104e2aba621ed7e1bcc39
thisisRahaf <60599531+thisisRahaf@users.noreply.github.com>
2020-09-29 21:40:38 +0300

    check password strength
    by using regular expression. now the password must have a length more than 7, at least one capital english letter and one small english letter

--- SignUpScreen.js
+++ SignUpScreen.js
@@ -26,6 +26,10 @@ export default class SignUpScreen extends Component {
 
     const Emailexpression = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([\t]*\r\n)?[\t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([\t]*\r\n)?[\t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
     const Emailcheck = Emailexpression.test(String(this.state.email).toLowerCase());
+
+    const strongPass = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");
+    const Passcheck = strongPass.test(this.state.password);
+
     if (this.state.email === '' || this.state.password.trim() === '' || this.state.displayName.trim() === '' || this.state.confirmPassord.trim() === '') {
       Alert.alert('', 'يجب تعبئة جميع الحقول',
         [{ text: 'حسناً', onPress: () => { console.log('Ok pressed.') } }],
@@ -34,8 +38,8 @@ export default class SignUpScreen extends Component {
       Alert.alert('', 'الرجاء ادخال البريد الإلكتروني بصيغة صحيحة',
         [{ text: 'حسناً', onPress: () => { console.log('Ok pressed.') } }],
       )
-    } else if (this.state.password.length < 8) {
-      Alert.alert('', 'الرجاء إدخال كلمة مرور تتكون من 8 خانات كحد أدنى',
+    } else if (Passcheck === false) {
+      Alert.alert('', 'يجب ان تتكون كلمة المرور من 8 خانات أو أكثر وحرف انجليزي كبير وحرف انجليزي صغير على الأقل',
         [{ text: 'حسناً', onPress: () => { console.log('Ok pressed.') } }],
       )
     } else if (this.state.password !== this.state.confirmPassord) {
