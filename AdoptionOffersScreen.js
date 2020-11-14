81c69c21b770ca0ba3376a6a39d6980775f5c8bc
HaneenG7 <70780385+HaneenG7@users.noreply.github.com>
2020-11-14 22:52:35 +0300

    Initial filter

--- AdoptionOffersScreen.js
+++ AdoptionOffersScreen.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react'
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, RefreshControl, Alert} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, RefreshControl, Alert, Modal, TouchableHighlight,Button} from 'react-native';
 import firebase from './firebase';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 import { faComments} from '@fortawesome/free-solid-svg-icons';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import ToggleSwitch from 'toggle-switch-react-native' //COPY Status-----------------------------
+import { faFilter } from '@fortawesome/free-solid-svg-icons';
+import PopupDialog from 'react-native-popup-dialog';
+import { Checkbox } from 'react-native-paper';
 
 var AdoptionPostsData= [];
 
@@ -13,12 +17,84 @@ export default class AdoptionOffersScreen extends Component {
           super(props);
           this.state = { 
             refreshing: false,
+            modalVisible: false,
+            // Animal type check
+            isCat: false,
+            ifFish: false,
+            isDog: false,
+            isRabbit: false,
+            isBird: false,
           }
         }
+
+
         _onRefresh = () => {
           setTimeout(() => this.setState({ refreshing: false }), 1000);
         }
 
+
+
+//------------------------------------------------------------------------------
+ToggleOnOrOff = (offerStatus) => {
+  if (offerStatus === 'مغلق'){
+    return false;
+  } else if (offerStatus === 'متاح'){
+    return true;
+  }
+}
+
+
+ToggleDisable = (offerStatus) => {
+  if (offerStatus === 'مغلق'){
+    return true;
+  }else  if (offerStatus === 'متاح'){
+    return false;
+  }
+}
+
+
+onToggle = (isOn,offerStatus,postid) => {
+  if (offerStatus === 'مغلق'){
+    Alert.alert('', 'هذا العرض مغلق ولا يمكن إعادة إتاحته من جديد.',[{ text: 'حسناً'}])
+    console.log("Do nothing")  
+  }
+  else if (offerStatus === 'متاح'){
+    Alert.alert(
+      "",
+      "هل تود إغلاق هذا العرض؟",
+      [
+        {
+          text: "لا",
+          onPress: () => console.log("لا"),
+          style: "cancel"
+        },
+        { text: "نعم", onPress: () => this.CloseOffer(postid) }
+      ],
+      { cancelable: false }
+    );
+    }
+}
+
+CloseOffer = (postid) => {
+  firebase.database().ref('/AdoptionPosts/'+postid).update({
+    offerStatus: 'مغلق'
+  }).then((data) => {
+    this.readPostData(); 
+    Alert.alert('', 'لقد تم إغلاق عرض التبني بنجاح, الرجاء تحديث صفحة عروض التبني',[{ text: 'حسناً'}])
+  });
+}
+//------------------------------------------------------------
+
+        filter = () =>{
+          console.log('Hi')
+          AdoptionPostsData=[]
+          console.log(AdoptionPostsData)
+          return (
+            <View >
+              <Text>تصفية حسب اkgklkj;jdf;er;oigh;odgj;snb;kbnkdfsjb;dfbodfivbkjdsnvkdjfbvszdujvbk;jfdbvzsudvh;ksdzbلنوع</Text>
+            </View>
+          );
+        }
         onPressTrashIcon = (postid) => {
           Alert.alert(
             "",
@@ -35,21 +111,28 @@ export default class AdoptionOffersScreen extends Component {
           );
         }
 
-        onPressDelete = (postid) => { // start new method
+
+        onPressDelete = (postid) => {
           AdoptionPostsData=AdoptionPostsData.filter(item => item.postid !== postid)
           firebase.database().ref('/AdoptionPosts/'+postid).remove().then((data) => {
             this.readPostData(); 
             Alert.alert('', 'لقد تم حذف عرض التبني بنجاح, الرجاء تحديث صفحة عروض التبني',[{ text: 'حسناً'}])
           });
-         }  //end new method
+         }
+
+
 
         AdoptionUpload = () => this.props.navigation.navigate('اضافة عرض تبني')
+
+
         onPressChatIcon = (offerorID , Name) => {
           this.props.navigation.navigate('صفحة المحادثة',{
             offerorID: offerorID,
             name: Name
           })
         }
+
+
         readPostData =() => {
             var ref = firebase.database().ref("AdoptionPosts");
             ref.on('value',  function (snapshot) {
@@ -67,6 +150,7 @@ export default class AdoptionOffersScreen extends Component {
              }
           //------------------------------------------------------------------------    
               var postKeys = Object.keys(post);// to find the post keys and put them in an array
+              var name;
               for(var i = 0; i< postKeys.length;i++){
                 var postInfo = postKeys[i];
                 //---------This to save the post info in variables----------
@@ -78,6 +162,10 @@ export default class AdoptionOffersScreen extends Component {
                 var UserName = post[postInfo].uName;
                 var offerorID = post[postInfo].userId; 
                 var postidentification = postInfo;  
+                var Status = post[postInfo].offerStatus;//COPY Status------------------------------
+                firebase.database().ref('account/'+offerorID+'/name').on('value',snapshot=>{
+                  name= snapshot.val()
+                })
                 //----------------Adoption Posts Array-----------------------
                 AdoptionPostsData[i]={
                   AnimalType: AniType,
@@ -85,9 +173,10 @@ export default class AdoptionOffersScreen extends Component {
                   AnimalAge: AniAge,
                   AnimalCity: AniCity,
                   AnimalPic: AniPic,
-                  Name: UserName,
+                  Name: name,
                   offerorID: offerorID,
-                  postid: postidentification
+                  postid: postidentification,
+                  offerStatus: Status,//COPY Status------------------------------
                 }  
               }         
             }); 
@@ -104,17 +193,37 @@ export default class AdoptionOffersScreen extends Component {
                     <Text style={styles.text}>{"جنس الحيوان: "+element.AnimalSex}</Text>
                     <Text style={styles.text}>{"عمر الحيوان: "+element.AnimalAge}</Text>
                     <Text style={styles.text}>{"المدينة: "+element.AnimalCity}</Text>
+                    <Text style={styles.text}>{"حالة العرض: "+element.offerStatus}</Text>
+                    
+                    <View style={{flexDirection: 'row'}}>
                     <TouchableOpacity 
                      style={styles.iconStyle}
                      onPress={()=> this.onPressTrashIcon(element.postid)}>
                      <FontAwesomeIcon icon={ faTrashAlt }size={30} color={"#69C4C6"}/>
                     </TouchableOpacity>
+
+                    <View style={styles.toggleStyle}>
+                    <ToggleSwitch
+                    isOn= {this.ToggleOnOrOff(element.offerStatus)}
+                    onColor="green"
+                    offColor="red"
+                    label="إغلاق العرض"
+                    labelStyle={{ color: "black", fontWeight: "900" }}
+                    size="small"
+                    onToggle={isOn => {
+                      this.onToggle(isOn,element.offerStatus,element.postid);
+                    }}
+                    disable={this.ToggleDisable(element.offerStatus)}
+                    />
+                    </View>
+                    </View>
+
                   </View>
                   </View>
                   
                 );
               }
-              else{
+              else if (element.offerStatus === 'متاح'){
               return (
                 <View style={{ marginBottom:30}}>
                   <View style={styles.Post}>
@@ -125,6 +234,7 @@ export default class AdoptionOffersScreen extends Component {
                   <Text style={styles.text}>{"جنس الحيوان: "+element.AnimalSex}</Text>
                   <Text style={styles.text}>{"عمر الحيوان: "+element.AnimalAge}</Text>
                   <Text style={styles.text}>{"المدينة: "+element.AnimalCity}</Text>
+                  <Text style={styles.text}>{"حالة العرض: "+element.offerStatus}</Text>
                   <Text/>
                   <TouchableOpacity 
                   style={styles.iconStyle}
@@ -132,16 +242,34 @@ export default class AdoptionOffersScreen extends Component {
                   <FontAwesomeIcon icon={ faComments }size={36} color={"#69C4C6"}/>
                 </TouchableOpacity>
                 <Text/>
-              
+
                 </View>
                 </View>
                 
               );}
+              else{
+                return (
+                  <View style={{ marginBottom:30}}>
+                    <View style={styles.Post}>
+                    <Image style={{ width: 290, height: 180 ,marginLeft:10, marginTop:12,}}
+                      source={{uri: element.AnimalPic}}/>
+                      <Text style={styles.text}>{"اسم صاحب العرض: "+element.Name}</Text>
+                    <Text style={styles.text}>{"نوع الحيوان: "+element.AnimalType}</Text>
+                    <Text style={styles.text}>{"جنس الحيوان: "+element.AnimalSex}</Text>
+                    <Text style={styles.text}>{"عمر الحيوان: "+element.AnimalAge}</Text>
+                    <Text style={styles.text}>{"المدينة: "+element.AnimalCity}</Text>
+                    <Text style={styles.text}>{"حالة العرض: "+element.offerStatus}</Text>
+                    <Text/>
+                  <Text/>
+                  </View>
+                  </View>
+                );}
             }).reverse();
         }
 
         render(){ 
               return (
+                
                 <ScrollView style={{ backgroundColor:'#FFFCFC' }}
                  refreshControl={
                   <RefreshControl
@@ -163,7 +291,86 @@ export default class AdoptionOffersScreen extends Component {
                        style={styles.button}>
                     <Text style={styles.textStyle}>اضافة عرض تبني</Text>
                     </TouchableOpacity>
+       
+                    <TouchableOpacity 
+                     style={styles.iconStyle}
+                     onPress={()=> { this.setState({ modalVisible: true})}}>
+                     <FontAwesomeIcon icon={ faFilter }size={30} color={"#69C4C6"}/>
+                    </TouchableOpacity>
                     {this.readPostData()} 
+                    
+                    <Modal
+        animationType="slide"
+        transparent={true}
+        visible={this.state.modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+        }}>
+          
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>تصفية حسب نوع الحيوان</Text>
+            
+            <View style={styles.ModalCon}>
+              <Text>قطط</Text>
+              <Checkbox
+              title='optForReceipts'
+              status={this.state.isCat ? 'checked' : 'unchecked'}
+              onPress={() => { this.setState({ isCat: !this.state.isCat }); }}
+            />
+            </View>
+
+            <View style={styles.ModalCon}>
+              <Text>كلاب</Text>
+              <Checkbox
+              title='optForReceipts'
+              status={this.state.isDog ? 'checked' : 'unchecked'}
+              onPress={() => { this.setState({ isDog: !this.state.isDog }); }}
+            />
+            </View>
+            
+            <View style={styles.ModalCon}>
+              <Text>عصافير</Text>
+              <Checkbox
+              title='optForReceipts'
+              status={this.state.isBird ? 'checked' : 'unchecked'}
+              onPress={() => { this.setState({ isBird: !this.state.isBird }); }}
+            />
+            </View>
+
+            <View style={styles.ModalCon}>
+              <Text>أرانب</Text>
+              <Checkbox
+              title='optForReceipts'
+              status={this.state.isRabbit ? 'checked' : 'unchecked'}
+              onPress={() => { this.setState({ isRabbit: !this.state.isRabbit }); }}
+            />
+            </View>
+
+            <View style={styles.ModalCon}>
+              <Text>سمك</Text>
+              <Checkbox
+              title='optForReceipts'
+              status={this.state.ifFish ? 'checked' : 'unchecked'}
+              onPress={() => { this.setState({ ifFish: !this.state.ifFish }); }}
+            />
+            </View>
+
+            <TouchableHighlight
+              style={{ ...styles.openButton, backgroundColor: '#2196F3' }}
+              onPress={() => {
+                this.setState({
+                  modalVisible: !this.state.modalVisible
+                })
+                {this.filter()}
+              }}>
+              <Text style={styles.textStyle}>تصفية</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+      
+
                 </View>
                 </ScrollView>
             );
@@ -227,13 +434,70 @@ const styles = StyleSheet.create({
   },
   iconStyle: {
     padding:8,
-    left: 30
+    left: 30,
   },
-//--------------------------------------
  mandatoryTextStyle: { 
   color: 'red',
   fontSize: 13,
   marginTop: 5,
+  },
+  //-----------------------------------
+    toggleStyle: {
+      padding:8,
+      left: 110,
+      paddingTop: 10,
+    },
+  //----------------------------------
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    height: 650,
+    width: 350,
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  openButton: {
+    backgroundColor: '#F194FF',
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
+  dialogContentView: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+  },
+  button_1: {
+    width: '40%',
+    height: 30,
+  },
+
+  ModalCon: {
+    flexDirection: 'row',
+    alignItems: 'center',
   }
-///--------------------------------------
 });
